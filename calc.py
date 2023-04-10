c6060637026df0f67b3404e397ecc35f0f7d40c4
minzu <ryube32@naver.com>
2023-04-10 14:41:28 +0900

    2020508034 commit

--- calc.py
+++ calc.py
@@ -2,13 +2,19 @@
 # return sum
 def plus(x, y):
     return x+y
+def minus(x,y):
+    return x-y
+def double(x,y):
+    return x*y
+def division(x,y):
+    x//y
 
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
     while check >= 1:        
-        print("0: exit, 1: plus")
+        print("0: exit, 1: plus 2:minus 3: double 4:division")
         check = int(input())
         if check == 1:
             print("First Number")
@@ -16,8 +22,24 @@ def main():
             print("Second Number")
             y = int(input())
             print("answer : ", plus(x,y))
-        elif check > 1:
-            print("Unsupported")
+        elif check == 2:
+             print("First Number")
+             x = int(input())
+             print("Second Number")
+             y = int(input())
+             print("answer : ", minus(x,y))
+        elif check == 3:
+             print("First Number")
+             x = int(input())
+             print("Second Number")
+             y = int(input())
+             print("answer : ", double(x,y))
+        elif check == 4:
+             print("First Number")
+             x = int(input())
+             print("Second Number")
+             y = int(input())
+             print("answer : ", division(x,y))
         else:
             print("Thank you")
 
